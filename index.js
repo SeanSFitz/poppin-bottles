b690f4d5f7ac541860d2fc35a2c26e22ac55302c
SeanSFitz <sean.s.fitz@gmail.com>
2017-06-03 21:28:16 +0000

    Complete

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log(`${wallet.bottlesPurchased} of these bottles were from the initial p
 console.log(`${wallet.bottlesFromCaps} of these bottles were from redeeming caps.`);
 console.log(`${wallet.bottlesFromEmpties} of these bottles were from redeeming empties.`);
 
-console.log(`\nYou currently have:`);
+console.log(`\nYou will have the following amounts remaining after redeeming all your caps and empties:`);
 console.log(`${wallet.dollars} dollars`);
 console.log(`${wallet.caps} caps`);
 console.log(`${wallet.empties} empties`);
